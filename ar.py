44f2213d4732e2cf960a79fbab68b08f7bcb33c8
Erik <ebz@bth.se>
2017-01-03 14:44:44 +0100

    Add probing network

--- ar.py
+++ ar.py
@@ -14,8 +14,23 @@ class Network:
 	size=""
 	interface=""
 
-	def setIp(self,ip):
-		self.ip = socket.:
+	def int2addr(self, _addr):
+		return socket.inet_ntoa(struct.pack("!I",_addr))
+
+	def addr2int(self,_addr):
+		return struct.unpack("!I",socket.inet_aton(_addr))[0]
+
+	def setIp(self,_ip):
+		self.ip = self.int2addr(_ip)
+
+	def getIp(self):
+		return self.addr2int(self.ip)
+
+	def setMask(self,_netmask):
+		self.netmask = self.int2addr(_netmask)
+
+	def isValid(self):
+		return self.ip and self.netmask and self.gateway
 
 class Host:
 	ip = ""
@@ -50,8 +65,8 @@ def poisonArp(interface,spoofIp,targetIp):
 			"&"]
 	subprocess.Popen(cmd);
 
-def getNetworkSize(netmask):
-	netmask = netmask.split(".")
+def getNetworkSize(_netmask):
+	netmask = _netmask.split(".")
 	binStr = ""
 	for octet in netmask:
 		binStr += bin(int(octet))[2:].zfill(8)
@@ -61,19 +76,23 @@ def getNetworkSize(netmask):
 def getNetwork(_interface):
 	network = Network()
 	network.interface = _interface
-	for iface in config.route.routes:
+	for iface in conf.route.routes:
 		if iface[3] == network.interface:
-			if iface[2] != "0.0.0.0"
+			if iface[2] == "0.0.0.0":
 				network.gateway = iface[2]
 			if iface[0] != 0:
 				network.setIp(iface[0])
 			if iface[1] != 0:
 				network.setMask(iface[1])
+	if network.isValid():
+		network.size = getNetworkSize(network.netmask)
+	else:
+		network = False
 	return network
 
 def probeNetwork(_network):
 	# Probe network for available devices
-	p = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdsf=_network.ip+"/"+_network.size)
+	p = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=_network.ip+"/"+_network.size)
 	ans,unans = srp(p,timeout=2)
 	return ans
 
@@ -83,18 +102,21 @@ def listDevices(_network):
 	res = -1
 	list = "[-] No devices found on network\n"
 	if len(devices) > 0:
-		list =	"+-------------------+-------------+\n" +\
-				"| MAC               | IP          |\n" +\
-				"+-------------------+-------------+\n"
+		list =	'''
++-------------------+----------------+
+| {:17s} | {:14s} |
++-------------------+----------------+\n'''.format("MAC","IP")
 		for device in devices:
-			list = list + "| {} | {} |\n".format(device[1].hwsrc,device[1].psrc)
-			list = list + "+-------------------+-------------+\n"
+			list = list + "| {:17s} | {:14s} |\n".format(device[1].hwsrc,device[1].psrc)
+			list = list + "+-------------------+----------------+\n"
 		res = 1
 	return list,res
 
 
 def main():
 	# TODO: Check if progam is run as root
+	if os.getuid() != 0:
+		err("Program must be ran as root",1)
 	parser = initArgs();
 	if len(sys.argv) < 2:
 		parser.print_help()
@@ -103,8 +125,10 @@ def main():
 	if not args.interface:
 		err("No interface specified",1)
 	network = getNetwork(args.interface)
+	if not network:
+		err("Interface {} doesn't seem to be connected".format(args.interface),1)
 	if args.list:
-		list,res = listDevices();
+		list,res = listDevices(network);
 		if res == 0:
 			print list
 			sys.exit(0)
